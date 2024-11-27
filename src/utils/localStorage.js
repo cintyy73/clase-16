95d96793e3efc80ee533d4525366c25b4e91d18e
cintyy73 <cintiapaezdm@gmail.com>
2024-11-27 08:09:46 -0300

    ejercicios

---
+++ src/utils/localStorage.js
@@ -0,0 +1,8 @@
+export const setLocalStorage = (key, data)=>{
+    localStorage.setItem(key, JSON.stringify(data))
+}
+
+export const getLocalStorage = (key)=>{
+    return JSON.parse( localStorage.getItem( key ))
+}
+
